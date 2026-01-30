8a60933ec0dc18be45d26ec98f5a7cf262e89547
daegeonyu45-sketch <daegeonyu45@gmail.com>
2026-01-30 17:36:29 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. 검사 기능 끄기 (기존)
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  // 2. 폰트 다운로드 에러 무시 (핵심!)
-  optimizeFonts: false,
 };
 
 export default nextConfig;
